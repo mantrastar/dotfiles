1dba1d08825e9422618ba3bf415a604242d953b2
Ven Jandhyala <ven@mantra.io>
2012-09-06 18:27:35 -0700

    Rename config.zsh to base.zsh

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,7 +1,3 @@
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-export LS_COLORS="di=1;36;1:ln=35;1:so=32;1:pi=33;1:ex=31;1:bd=34;36:cd=34;33:su=0;31:sg=0;36:tw=0;32:ow=0;33:"
-export CLICOLOR=true
-
 HISTFILE=$DOT/zsh/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
@@ -23,3 +19,8 @@ setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY SHARE_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
+
+# Directory switching
+cdpath=(~ ~/Projects ~/Projects/Mantra ~/Projects/Spacebase ~/Projects/Self ~/Projects/Learning ~/Projects/Random)
+setopt autopushd
+setopt autocd
