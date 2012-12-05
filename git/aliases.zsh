5e0e1db966622c12b8e19a9a527c45db35764944
Ven Jandhyala <ven@mantra.io>
2012-12-05 05:07:30 -0800

    Update git aliases with quick add and commit

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use hub as git wrapper
-# https://github.com/defunkt/hub
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
-
 alias gs='git status -sb'
 logfmt='%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset'
 alias glg="git log --graph --pretty=format:'$logfmt' --abbrev-commit --date=relative"
@@ -21,3 +13,25 @@ alias gph='git push heroku master'
 alias gf='git fetch origin'
 alias gm='git merge'
 alias gcl='git clone'
+
+gac() {
+  # Quickly add and commit in one step, with default commit message.
+
+  # Check number of arguments.
+  if [ $# -eq 1 ]
+  then
+    message="Work on $1"
+  elif [ $# -gt 1 ]
+  then
+    message="Work"
+  else
+    echo 'Provide file or pattern to add and commit.'
+    return 1
+  fi 
+ 
+  # Add.
+  ga $@
+
+  # Commit.
+  gc -m $message $@
+}
